c805a5eea4cdf9c4847155a9cd47afbeaa51d2e6
Abu-Isa <sulaimanabdulrahman92@gmail.com>
2026-09-04 16:19:43 +0100

    Add calculator project

---
+++ demo8_calculator.py
@@ -0,0 +1,46 @@
+while True:
+    first_no= input ("Enter first number: ")
+    if first_no.lower()=="quit":
+        break
+    first= float(first_no)
+
+
+    while True:
+        operator=input ("Enter operator: ")
+        if operator.lower()=="quit":
+             break
+        second_no= input("Enter second number: ")
+        if second_no.lower()=="quit":
+             break
+        second=float(second_no)
+
+        if operator =="+":
+            res=first+second
+        elif operator =="-":
+            res=first-second
+        elif operator =="/":
+            res=first/second
+        elif operator =="//":
+            res=first//second
+        elif operator =="%":
+            res=first%second
+        elif operator =="*":
+            res=first*second   
+        else:
+            print("Invalid Input")
+            break  
+
+        print(res)
+
+        choise= input (f"continue with {res} ? (y/n)")
+        first=res
+        if choise== "y":
+            continue
+        else:
+            break
+
+
+
+#teaching method
+#mindset
+#reality
